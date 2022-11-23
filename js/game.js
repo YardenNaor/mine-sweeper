66b9ee6d1bc4a297a1f35f4c6f4782ec09a47fae
YardenNaor <118841631+YardenNaor@users.noreply.github.com>
2022-11-23 16:07:01 +0200

    ok

--- js/game.js
+++ js/game.js
@@ -1,88 +1,21 @@
 'use strict'
 
 
-// But as a guideline, we suggest having the following functions (it is
-//     ok to have more functions as needed).
-//    
-//    
-//   
-//     renderBoard(board) Render the board as a <table>
-//     to the page
-//     cellClicked(elCell, i, j) Called when a cell (td) is
-//     clicked
-//     cellMarked(elCell) Called on right click to mark a
-//     cell (suspected to be a mine)
-//     Search the web (and
-//     implement) how to hide the
-//     context menu on right click
-//     checkGameOver() Game ends when all mines are
-//     marked, and all the other cells
-//     are shown
-//     expandShown(board, elCell,
-//     i, j)
-//     When user clicks a cell with no
-//     mines around, we need to open
-//     not only that cell, but also its
-//     neighbors.
-//     NOTE: start with a basic
-//     implementation that only opens
-//     the non-mine 1st degree
-//     neighbors
-//     BONUS: if you have the time
-//     later, try to work more like the
-//     real algorithm (see description
-//     at the Bonuses section below)
-//     Here are the global variables you might be using:
-//     gBoard – A Matrix
-//     containing cell objects:
-//     Each cell: {
-//      minesAroundCount: 4,
-//      isShown: false,
-//      isMine: false,
-//      isMarked: true
-
-//     }
-//     The model
-//     gLevel = {
-//      SIZE: 4,
-//      MINES: 2
-//     };
-//     This is an object by which the
-//     board size is set (in this case:
-//     4x4 board and how many mines
-//     to put)
-//     gGame = {
-//      isOn: false,
-//      shownCount: 0,
-//      markedCount: 0,
-//      secsPassed: 0
-//     }
-//     This is an object in which you
-//     can keep and update the
-//     current game state:
-//     isOn: Boolean, when true we
-//     let the user play
-//     shownCount: How many cells
-//     are shown
-//     markedCount: How many cells
-//     are marked (with a flag)
-//     secsPassed: How many seconds
-//     passed 
-
-
-// initGame() This is called when page loads
 
 const MINE = '💣'
 const FLAG = '🏴‍☠️'
 
 
 var gBoard
+var gEmptyCellsIdx
 
 var gGame = {
     isOn: false,
     shownCount: 0,
     markedCount: 0,
-    secsPassed: 0
+    livesCount: 3,
+    secsPassed: 0,
+    steps: []
 }
 
 var gLevel = {
@@ -92,17 +25,16 @@ var gLevel = {
 
 
 function onGameInit() {
+    gGame.livesCount = 3
+    gGame.isOn = true
+    gGame.steps = []
+    gEmptyCellsIdx = []
     gBoard = buildBoard(4, 4)
-    // console.log('g:', gBoard)
-    setMines()
+    renderLife()
+    findEmptyCells(gBoard)
     renderBoard(gBoard)
 }
 
-
-// buildBoard() Builds the board
-//     Set mines at random locations
-//     Call setMinesNegsCount()
-//     Return the created board
 
 // 1. Create a 4x4 gBoard Matrix containing Objects. Place 2 mines
 // manually when each cell’s isShown set to true.
@@ -115,8 +47,8 @@ function buildBoard(ROWS, COLS) {
         mat[i] = []
         for (var j = 0; j < COLS; j++) {
             const cell = {
-                minesAroundCount: 4,
-                isShown: true,
+                minesAroundCount: null,
+                isShown: false,
                 isMine: false,
                 isMarked: false
             }
@@ -139,12 +71,13 @@ function renderBoard(board) {
             var img = ''
             if (cell.isShown) {
                 if (cell.isMine) img = MINE
+                else if (cell.minesAroundCount) img = cell.minesAroundCount
             } else {
                 if (cell.isMarked) img = FLAG
             }
-             strHTML += `<td data-i="${i}" data-j="${j}" onclick="onCellClicked(this, ${i}, ${j})">${img}</td>`
+            strHTML += `<td class="cell-${i}-${j}" onclick="onCellClicked(this,${i}, ${j})" onmouseup="cellMarked(this, i, j)">${img}</td>`
         }
-       
+
         strHTML += '</tr>'
     }
 
@@ -155,15 +88,20 @@ function renderBoard(board) {
 
 
 function setMines() {
-    for (var i = 0; i <= gLevel.MINES; i++) {
-        const randomCell = gBoard[getRandomInt(0, gBoard.length)][getRandomInt(0, gBoard[0].length)]
-        randomCell.isMine = true
+    console.log('empty:', gEmptyCellsIdx)
+    for (var i = 0; i < gLevel.MINES; i++) {
+        // const randomCell = gBoard[getRandomInt(0, gBoard.length)][getRandomInt(0, gBoard[0].length)]
+        const randomCellIdx = drawNum(gEmptyCellsIdx)
+        // console.log('cell:', randomCell)
+        gBoard[randomCellIdx.i][randomCellIdx.j].isMine = true
     }
+    console.log('after drawing:', gEmptyCellsIdx)
     // gBoard[2][0].isMine = true
     // gBoard[0][1].isMine = true
     // gBoard[2][0].isShown = true
     // gBoard[0][1].isShown = true
-    console.log('g:', gBoard)
+    // console.log('g:', gBoard)
+
 }
 
 
@@ -173,3 +111,124 @@ function setMines() {
 //     minesAroundCount.
 
 
+function findEmptyCells(board) {
+    // console.log('g:',gBoard)
+    for (var i = 0; i < board.length; i++) {
+        for (var j = 0; j < gBoard[0].length; j++) {
+            const cell = gBoard[i][j]
+            if (!cell.isMine && !cell.isShown) gEmptyCellsIdx.push({ i, j })
+            // console.log('empty:', gEmptyCellsIdx)
+
+        }
+    }
+    // console.log('empty at find:',emptyCellsIdx)
+}
+
+// Step2 – counting neighbors:
+// 1. Create setMinesNegsCount() and store the numbers
+// (isShown is still true)
+// 2. Present the board with the neighbor count and the mines
+// using renderBoard() function.
+// 3. Have a console.log presenting the board content – to help
+// you with debugging
+
+function setMinesNegsCount() {
+    // console.log('empty:', gEmptyCells)
+    for (var i = 0; i < gEmptyCellsIdx.length; i++) {
+        const cellPosI = gEmptyCellsIdx[i].i
+        const cellPosJ = gEmptyCellsIdx[i].j
+        gBoard[cellPosI][cellPosJ].minesAroundCount = countMinedNeighbors(cellPosI, cellPosJ, gBoard)
+        // console.log('g:', gBoard)
+    }
+}
+
+function countMinedNeighbors(cellI, cellJ, mat) {
+    var neighborsCount = 0
+    for (var i = cellI - 1; i <= cellI + 1; i++) {
+        if (i < 0 || i >= mat.length) continue
+
+        for (var j = cellJ - 1; j <= cellJ + 1; j++) {
+            if (i === cellI && j === cellJ) continue
+            if (j < 0 || j >= mat[i].length) continue
+
+            if (mat[i][j].isMine) neighborsCount++
+
+        }
+    }
+    return neighborsCount
+}
+
+// Step3 – click to reveal:
+// 1. When clicking a cell, call the cellClicked() function.
+// 2. Make the default “isShown” to be “false”
+// 3. Implement that clicking a cell with “number” reveals the
+// // number of this cell
+// Make sure the first clicked cell is never a mine (like in the real
+//     game)
+//     HINT: place the mines and count the neighbors only on first
+//     click.
+
+function onCellClicked(elCell, i, j) {
+    var img = ''
+    const cell = gBoard[i][j]
+    if (!gGame.isOn) return
+    if (cell.isMarked) return
+    if (!cell.isShown) {
+        gGame.steps.push({ i, j })
+        cell.isShown = true
+        gGame.shownCount++
+        // console.log('shown:', gGame.shownCount)
+    }
+    if (gGame.steps.length === 1) {
+        setMines()
+        setMinesNegsCount()
+    }
+
+    if (cell.isMine) {
+        img = MINE
+        takeLife()
+    }
+    else if (cell.minesAroundCount) img = cell.minesAroundCount
+    elCell.innerText = img
+
+}
+
+// Add support for “LIVES” -
+// The user has 3 LIVES:
+// When a MINE is clicked, there is an indication to the user that
+// he clicked a mine. The LIVES counter decreases. The user can
+// continue playing .
+
+function takeLife() {
+    gGame.livesCount--
+    if (!gGame.livesCount) gameOver()
+    renderLife()
+}
+
+function renderLife() {
+    const elLife = document.querySelector('.life')
+    // console.log('div:', elLife)
+    elLife.innerHTML = `You have ${gGame.livesCount} lifes`
+}
+
+
+function gameOver() {
+    gGame.isOn = false
+    const elbtn = document.querySelector('.smily')
+    elbtn.innerHTML = '🙊'
+}
+
+
+// Add smiley (feel free to switch icons \ images):
+// ● Normal 😃
+// ● Sad & Dead – LOSE 🤯 (stepped on a mine)
+// ● Sunglasses – WIN 😎
+// ● Clicking the smiley should reset the game
+
+function cellMarked(elCell) {
+console.log('hi hi')
+//    elCell.addEventListener('contextmenu', (event) => {
+//         gBoard[i][j].isMarked = !gBoard[i][j].isMarked
+//         elCell.innerHTML = FLAG
+//     })
+}
--- js/utils.js
+++ js/utils.js
@@ -16,20 +16,7 @@ function getClassName(location) {
     return cellClass
 }
 
-function countNeighbors(cellI, cellJ, mat) {
-    var neighborsCount = 0
-    for (var i = cellI - 1; i <= cellI + 1; i++) {
-        if (i < 0 || i >= mat.length) continue
 
-        for (var j = cellJ - 1; j <= cellJ + 1; j++) {
-            if (i === cellI && j === cellJ) continue
-            if (j < 0 || j >= mat[i].length) continue
-
-            if (mat[i][j] === LIFE || mat[i][j] === SUPER_LIFE) neighborsCount++
-        }
-    }
-    return neighborsCount
-}
 
 
 function startTimer() {
@@ -45,4 +32,11 @@ function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function drawNum(nums) {
+    var randIdx = getRandomInt(0, nums.length)
+    var num = nums[randIdx]
+    nums.splice(randIdx, 1)
+    return num
 }
